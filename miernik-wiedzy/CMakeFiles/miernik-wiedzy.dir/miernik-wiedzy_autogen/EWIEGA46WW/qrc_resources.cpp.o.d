6ad25c436da30a6a3c5211e6159fdcf6ba188838
ZMejer <zosiacastiel@gmail.com>
2023-11-10 10:53:50 +0100

    usuwanie plikow/folderow uwzglednionych w .gitignore

--- miernik-wiedzy/CMakeFiles/miernik-wiedzy.dir/miernik-wiedzy_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/miernik-wiedzy.dir/miernik-wiedzy_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
-  /Users/zofia/Studia/Projekt-C++/PCPP2023-miernik-wiedzy/miernik-wiedzy/miernik-wiedzy_autogen/EWIEGA46WW/qrc_resources.cpp
